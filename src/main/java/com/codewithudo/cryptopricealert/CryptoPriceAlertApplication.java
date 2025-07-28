989e7ddd8b23867db026656ef7428d0343a527de
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-28 09:29:52 +0100

    Enabled Scheduling

--- src/main/java/com/codewithudo/cryptopricealert/CryptoPriceAlertApplication.java
+++ src/main/java/com/codewithudo/cryptopricealert/CryptoPriceAlertApplication.java
@@ -2,8 +2,10 @@ package com.codewithudo.cryptopricealert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CryptoPriceAlertApplication {
 
     public static void main(String[] args) {
